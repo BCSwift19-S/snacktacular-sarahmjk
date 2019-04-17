9b6a3df8e09254bc028d260dd8eb9b6ac5d9708a
Sarah Kim <sarah.mjk@hotmail.com>
2019-04-17 17:42:04 -0400

    Initial Commit

---
+++ Snacktacular/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snacktacular
+//
+//  Created by Sarah Minji Kim on 4/17/19.
+//  Copyright © 2019 Sarah Minji Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
